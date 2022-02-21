3a41f1fa59ed9d4770d554ce9f0940e658b26848
hara prasad <haraprasad45@gmail.com>
2022-02-22 01:35:22 +0530

    destination moon done and bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,17 @@ module.exports = {
       bellefair: ["Bellefair", "sans-serif"],
     },
     extend: {
+      colors: {
+        "space-black": "#0B0D17",
+        "space-purple": "#D0D6F9",
+      },
       backgroundImage: {
         "hero-bg": "url('/assets/home/background-home-desktop.jpg')",
-        "hero-bg-mobile": "url('/assets/home/background-home-mobile.jpg')"
+        "hero-bg-mobile": "url('/assets/home/background-home-mobile.jpg')",
+        "destination-desktop":
+          "url('/assets/destination/background-destination-desktop.jpg')",
+        "destination-mobile":
+          "url('/assets/destination/background-destination-mobile.jpg')",
       },
     },
   },
